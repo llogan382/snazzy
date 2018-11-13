02821a90512e7498042309e03a256d2ff21c05d9
Unknown <llogan382@gmail.com>
2018-11-13 11:24:01 -0500

    Adds custom query for only posts

--- search.php
+++ search.php
@@ -16,27 +16,29 @@ get_header(); ?>
 	<main id="main" class="site-main site-main--wrapper" role="main">
 
 		<section id="hfeed" class="hfeed">
-
-			<?php if ( have_posts() ) : ?>
+		<?php $query = new WP_Query( array( 'post_type' => 'post' ) );
+				?>
+			<?php if ( $query->have_posts() ) : ?>
 
 				<header class="entry-header">
 					<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', '@@textdomain' ), '<span>' . esc_html( get_search_query() ) . '</span>' ); ?></h1>
 				</header>
 
 				<?php
-				while ( have_posts() ) :
-					the_post();
+				while ( $query->have_posts() ) :
+					$query->the_post();
 
 					/*
 		               		 * Include the Post-Format-specific template for the content.
 		                	 * If you want to override this in a child theme, then include a file
 		                	 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 		                	*/
-					get_template_part( 'template-parts/content', get_post_format() );
+					get_template_part( 'template-parts/content-page', get_post_format() );
 
 				endwhile;
+				wp_reset_postdata();
 
-				if ( have_posts() ) :
+				if ( $query->have_posts() ) :
 					/**
 					 * Previous/next page navigation or JetPack infinite scroll, if active.
 					 * The following function is located in inc/template-tags.php
