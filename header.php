18126923812a16b6eebb0110733e470244ca0df2
Unknown <llogan382@gmail.com>
2018-11-13 12:17:03 -0500

    Includes google fonts

--- header.php
+++ header.php
@@ -14,6 +14,8 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js no-svg">
 <head>
+	<!-- For Child Theme Customization -->
+	<link href="https://fonts.googleapis.com/css?family=Montserrat|Playfair+Display" rel="stylesheet">
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
